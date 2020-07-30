c17d8d87166c2c05d46e57e81dfb11bf4010f487
Coursera Learner <nobody@nowhere.nul>
2020-07-30 06:45:34 +0000

    tst

---
+++ 22_tests_power/test-power.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+unsigned power (unsigned x, unsigned y);
+int main() {
+  bool check = true;
+  if(power(2,0)!=1)
+    return EXIT_FAILURE;
+  if(power(9,1)!=9)
+    return EXIT_FAILURE;
+   if(power(0,0)!=1)
+    return EXIT_FAILURE;
+  if(power(2,2)!=4)
+    return EXIT_FAILURE;
+   if(power(0,1)!=0)
+    return EXIT_FAILURE;
+    if(power(-1,6)!=1)
+    return EXIT_FAILURE;
+    if(power(-1,3)!=-1)
+    return EXIT_FAILURE;
+    if(power(0,-2)!=0)
+    return EXIT_FAILURE;
+  return EXIT_SUCCESS;
+}
