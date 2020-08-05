828a71dc5aa1a3f907b9cd39661b4b4f519fc6ba
Coursera Learner <nobody@nowhere.nul>
2020-08-05 05:41:02 +0000

    a

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,9 +16,51 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void printSorted(FILE *f) {
+  size_t size =0;
+  char *line =NULL;
+  char **array=NULL;
+  int i=0;
+  while(getline(&line,&size,f)>=0) {
+    array = realloc(array,(i+1)*sizeof(*array));
+    array[i]=line;
+    line=NULL;
+    i++;
+  }
+  free(line);
+  sortData(array,i);
+  for(int j=0;j<i;j++) {
+    printf("%s",array[j]);
+    free(array[j]);
+  }
+  free(array);
+}
+
 int main(int argc, char ** argv) {
   
   //WRITE YOUR CODE HERE!
-  
-  return EXIT_SUCCESS;
+  if(argc==1){
+    FILE *f=stdin;
+    if(f==NULL){
+      fprintf(stderr,"ERROR: could not open file\n");
+      return EXIT_FAILURE;
+    }
+    printSorted(f);
+  }
+  if(argc >1) {
+    for(int i=0;i<argc;i++) {
+      FILE *f= fopen(argv[i],"r");
+      if(f==NULL) {
+	fprintf(stderr,"ERROR: could not open file \n");
+	return EXIT_FAILURE;
+      }
+      printSorted(f);
+      if(fclose(f)!=0) {
+	perror("Could not close input file");
+	return EXIT_FAILURE;
+      }
+    }
+
+  }
+      return EXIT_SUCCESS;
 }
